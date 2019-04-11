268863a3c4fd8fb30c34eaffdf9dcefc598ba542
杨杉 <yangshan08@meituan.com>
2019-04-11 12:36:53 +0800

    abPath

--- index.js
+++ index.js
@@ -2,21 +2,24 @@
 const express = require('express')
 const app = express()
 const Luna = require('./lib/core')
+let prjPath = '/Users/yangshan/Documents/repo/mall-fe-pc-product/src'
+
 const open = require('open')
-const ln = new Luna('src')
+// const ln = new Luna('src')
+const ln = new Luna({ dir: prjPath, abPath: true })
 const path = require('path')
 const _envPath = __dirname
-
+// console.log('__dirname = ', __dirname)
 //全局变量缓存数据
 let result = {}
 app.use(express.static(path.join(_envPath, 'dist')))
 app.get('/getData', (req, res) => {
-  if(result.pages) {
+  if (result.pages) {
     res.send(result)
   } else {
     ln.getTree().then(rs => {
       res.send(rs)
-      result= rs;
+      result = rs;
     })
   }
 })
