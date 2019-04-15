301bc9818e53c45b690f78fe07cbb7142a4f369f
yangshan08 <yangshan08@meituan.com>
2019-04-15 08:53:29 +0800

    alias

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 #!/usr/bin/env node
-const express = require('express')
-const app = express()
-const Luna = require('./lib/core')
-let prjPath = '/Users/yangshan/Documents/repo/mall-fe-pc-product/src'
+const { statSync } = require('fs');
+const express = require('express');
+const app = express();
+const { basename, resolve } = require('path');
+const Luna = require('./lib/core');
+let prjPath = '/Users/yangshan/Documents/repo/mall-fe-pc-product/src';
 
-const open = require('open')
+const open = require('open');
 // const ln = new Luna('src')
-const ln = new Luna({ dir: prjPath, abPath: true })
-const path = require('path')
-const _envPath = __dirname
+const ln = new Luna({ dir: prjPath, abPath: true });
+const path = require('path');
+const _envPath = __dirname;
 // console.log('__dirname = ', __dirname)
 //全局变量缓存数据
-let result = {}
-app.use(express.static(path.join(_envPath, 'dist')))
+let result = {};
+app.use(express.static(path.join(_envPath, 'dist')));
 app.get('/getData', (req, res) => {
-  if (result.pages) {
-    res.send(result)
-  } else {
-    ln.getTree().then(rs => {
-      res.send(rs)
-      result = rs;
-    })
-  }
-})
+    if (result.pages) {
+        res.send(result);
+    } else {
+        ln.getTree().then(rs => {
+            res.send(rs);
+            result = rs;
+        });
+    }
+});
 
 app.listen(8081, () => {
-  open('http://localhost:8081/')
-})
+    open('http://localhost:8081/');
+});
